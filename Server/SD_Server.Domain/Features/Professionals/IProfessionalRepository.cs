10f7c4231fef9ed76598e677bcd6acae3a373640
flavia <flaviaandrader2011@gmail.com>
2026-04-10 08:56:03 -0300

    US-14 — [Backend] Criar entidade Professional no domínio

---
+++ Server/SD_Server.Domain/Features/Professionals/IProfessionalRepository.cs
@@ -0,0 +1,9 @@
+using SD_Server.Domain.Interfaces;
+
+namespace SD_Server.Domain.Features.Professionals
+{
+    public interface IProfessionalRepository : IRepositoryBase<Professional>
+    {
+
+    }
+}
